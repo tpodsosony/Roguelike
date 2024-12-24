ef752f08984d1f125164d5147bc55175e4e93c44
tpodsosony <talpods6@gmail.com>
2024-12-23 23:32:12 -0600

    Added AI for entities

--- components/ai.py
+++ components/ai.py
@@ -1,17 +1,23 @@
 from __future__ import annotations
 
-from typing import List, Tuple
+from typing import List, Tuple, TYPE_CHECKING
 
 import numpy as np # type: ignore
 import tcod
 
-from actions import Action
+from actions import Action, MeleeAction, MovementAction, WaitAction
 from components.base_component import BaseComponent
 
+if TYPE_CHECKING:
+    from entity import Actor
+
 class BaseAI(Action, BaseComponent):
+    entity: Actor
+
     def perform(self) -> None:
         raise NotImplementedError()
 
+    # Uses walkable tiles in map to get a path from parent entity
     def get_path_to(self, dest_x: int, dest_y: int) -> List[Tuple[int, int]]:
         """Compute and return a path to the target position.
 
@@ -30,13 +36,41 @@ class BaseAI(Action, BaseComponent):
                 # order to surround the player. 
                 cost[entity.x, entity.y] += 10
 
-            graph = tcod.path.SimpleGraph(cost=cost, cardinal=2, diagnol=3)
-            pathfinder = tcod.path.Pathfinder(graph)
+        graph = tcod.path.SimpleGraph(cost=cost, cardinal=2, diagonal=3)
+        pathfinder = tcod.path.Pathfinder(graph)
 
-            pathfinder.add_root((self.entity.x, self.entity.y)) # start position
+        pathfinder.add_root((self.entity.x, self.entity.y)) # start position
 
-            # Computer the path to the destination and remove the starting point
-            path: List[List[int]] = pathfinder.path_to((dest_x, dest_y))[1:].tolist()
+        # Computer the path to the destination and remove the starting point
+        path: List[List[int]] = pathfinder.path_to((dest_x, dest_y))[1:].tolist()
 
-            # Convert from List[List[int]] to List[Tuple[int, int]]
-            return [(index[0], index[1]) for index in path]
+        # Convert from List[List[int]] to List[Tuple[int, int]]
+        return [(index[0], index[1]) for index in path]
+
+class HostileEnemy(BaseAI):
+    def __init__(self, entity: Actor):
+        super().__init__(entity)
+        self.path: List[Tuple[int, int]] = []
+
+    def perform(self) -> None:
+        target = self.engine.player
+        dx = target.x - self.entity.x
+        dy = target.y - self.entity.y
+        distance = max(abs(dx), abs(dy)) # Cheyshev distance
+
+        # Checks to see if player is visible
+        if self.engine.game_map.visible[self.entity.x, self.entity.y]:
+            if distance <= 1: # Attacks player if next to them
+                return MeleeAction(self.entity, dx, dy).perform()
+
+            self.path = self.get_path_to(target.x, target.y) # Otherwise gets path to player location
+
+        # Moves one step towards player
+        if self.path:
+            dest_x, dest_y = self.path.pop(0)
+            return MovementAction(
+                self.entity, dest_x - self.entity.x, dest_y - self.entity.y,
+            ).perform()
+        
+        # Waits if entity is not visible to player
+        return WaitAction(self.entity).perform()
