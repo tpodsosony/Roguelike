d8b9a7b01ff7d80e8b1911cf8db6370d1a013428
tpodsosony <talpods6@gmail.com>
2025-03-24 19:41:19 -0500

    Added confusion scroll item, which puts a different ai on targeted npcs

--- input_handlers.py
+++ input_handlers.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Optional, TYPE_CHECKING
+from typing import Callable, Optional, Tuple, TYPE_CHECKING
 
 #importing classes
 import tcod
@@ -375,6 +375,19 @@ class LookHandler(SelectIndexHandler):
     def on_index_selected(self, x: int, y: int) -> None:
         """Return to main handler"""
         self.engine.event_handler = MainGameEventHandler(self.engine)
+
+class SingleRangedAttackHandler(SelectIndexHandler):
+    """Handles targeting a single enemy. Only the enemy selected will be affected"""
+
+    def __init__(
+            self, engine: Engine, callback: Callable[[Tuple[int, int]], Optional[Action]]
+    ):
+        super().__init__(engine)
+
+        self.callback = callback
+
+    def on_index_selected(self, x: int, y: int) -> Optional[Action]:
+        return self.callback((x, y))
 
         
 
