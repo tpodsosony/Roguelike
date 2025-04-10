477957a6cef386c43b866ffaa9368263d733e147
tpodsosony <talpods6@gmail.com>
2025-04-09 19:33:01 -0500

    Added a base event handler for either actions or event handlers

--- input_handlers.py
+++ input_handlers.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Callable, Optional, Tuple, TYPE_CHECKING
+from typing import Callable, Optional, Tuple, TYPE_CHECKING, Union
 
 #importing classes
 import tcod
@@ -60,6 +60,29 @@ CONFIRM_KEYS = {
     tcod.event.K_RETURN,
     tcod.event.K_KP_ENTER,
 }
+
+ActionOrHandler = Union[Action, "BaseEventHandler"]
+"""An event handler return value which can trigger an action or switch active handlers
+
+If a handler is returned then it will become the active handler for future events
+If an action is returnted it will be attmpted and if it's valid then
+MainGameEventHandler will become the active handler
+"""
+
+class BaseEventHandler(tcod.event.EventDispatch[ActionOrHandler]):
+    def handle_events(self, event: tcod.event.Event) -> BaseEventHandler:
+        """Handle an event and return the next active event handler"""
+        state = self.dispatch(event)
+        if isinstance(state, BaseEventHandler):
+            return state
+        assert not isinstance(state, Action), f"{self!r} can not handle actions."
+        return self
+    
+    def on_render(self, console: tcod.Console) -> None:
+        raise NotImplementedError()
+    
+    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
+        raise SystemExit()
 
 
 #Creating a subclass of tcods EventDispatch class
